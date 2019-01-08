8d1644e1fb09764cdda02ea63200cfd06d0522c9
ulanav <ulanav@rambler.ru>
2019-01-08 12:00:11 +0300

    repeat form size changed

--- WFA_test_GitVS2015_01/Form1.Designer.cs
+++ WFA_test_GitVS2015_01/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(394, 276);
+            this.ClientSize = new System.Drawing.Size(466, 207);
             this.Name = "Form1";
             this.Text = "FormTestGit01";
             this.ResumeLayout(false);
