4c421d2f8aa3e6b170704b400ce58dae623acacf
ulanav <ulanav@rambler.ru>
2019-01-08 13:11:40 +0300

    increase form size

--- WFA_test_GitVS2015_01/Form1.Designer.cs
+++ WFA_test_GitVS2015_01/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(357, 302);
+            this.ClientSize = new System.Drawing.Size(418, 347);
             this.Name = "Form1";
             this.Text = "FormTestGit01";
             this.ResumeLayout(false);
